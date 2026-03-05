ea10abd9ec1b2cdb0e18eaa3b204da5a191ef3d9
Torin Stanton-Andersson <161447267+Tottocs@users.noreply.github.com>
2026-03-05 20:26:48 +0000

    Update headers
    Draft

--- src/main-group2/include/ir-sensor-group2.h
+++ src/main-group2/include/ir-sensor-group2.h
@@ -1,3 +1,13 @@
-#ifndef IR_SENSOR_H
-#define IR_SENSOR_H
+#ifndef IR_SENSOR_GROUP2_H
+#define IR_SENSOR_GROUP2_H
 
+
+#ifndef N_SENSORS
+#define N_SENSORS 3
+#endif
+
+void Setup_IR_Sensors(int arr[N_SENSORS]);
+int Update_Direction(int* LeftMotorSpeed, int* RightMotorSpeed);
+int Scan();
+
+#endif
--- src/main-group2/include/motor-group2.h
+++ src/main-group2/include/motor-group2.h
@@ -1,2 +1,12 @@
 #ifndef MOTOR_GROUP2_H
 #define MOTOR_GROUP2_H
+
+
+void Setup_Main_Motors(	
+	int MOT_A1_PIN, int MOT_A2_PIN, 
+	int MOT_B1_PIN, int MOT_B2_PIN);
+void Set_Motor_pwm(int pwm, int IN1_PIN, int IN2_PIN);
+void Set_Motor_Currents(int pwm_A, int pwm_B);
+void Spin_And_Wait(int pwm_A, int pwm_B, int duration);
+
+#endif
