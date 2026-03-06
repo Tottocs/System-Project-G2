fb6bcda447a96fea5f2d487f4307385a0255cf0c
Torin Stanton-Andersson <161447267+Tottocs@users.noreply.github.com>
2026-03-06 22:06:41 +0000

    Update ir-sensor-group2.h
    Minor correction

--- src/main-group2/include/ir-sensor-group2.h
+++ src/main-group2/include/ir-sensor-group2.h
@@ -3,7 +3,7 @@
 
 #define N_SENSORS 3
 
-void Setup_IR_Sensors(int arr[N_SENSORS]);
+void Setup_IR_Sensors(int Arr[N_SENSORS]);
 void Update_Direction(int* LeftMotorSpeed, int* RightMotorSpeed);
 int Scan();
 
