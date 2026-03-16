ee41daa5272f3b84ed449ecafd252031c01493cb
Torin Stanton-Andersson <161447267+Tottocs@users.noreply.github.com>
2026-03-16 18:59:13 +0000

    Update scanner-reciever-group2.ino
    Testing failed but the code was not the problem

--- src/scanner-reciever-group2/scanner-reciever-group2.ino
+++ src/scanner-reciever-group2/scanner-reciever-group2.ino
@@ -4,43 +4,65 @@
 
 #define UART_BAUDRATE 9600
 #define INT_PIN 2
+#define SERIAL_TIMEOUT 100
 
 #define START_MSG 255
 #define END_MSG 254
 #define ACK_MSG 253
+#define UNREC_MSG 252
 
 #define RANGE_DEGREES 180
 
-byte Arr[];
+#define BLINK_DELAY 700
+
+byte Arr[180];
 
 void setup() {
   Serial.begin(UART_BAUDRATE);
   pinMode(INT_PIN, INPUT);
-  pinMode(LED_BUILT_IN, OUTPUT);
-  attachInterrupt(dgitalToInterrupt(INT_PIN),ISR_Handler,HIGH);
+  pinMode(LED_BUILTIN, OUTPUT);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN),ISR_Handler,HIGH);
 }
 
 void loop() {
   digitalWrite(LED_BUILTIN, HIGH);
-  delay(100);
-  digitalWite(LED_BUILTIN, LOW);
-  delay(100);
+  delay(BLINK_DELAY);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(BLINK_DELAY);
 }
 
 void ISR_Handler() {
-  Read_Serial();
+  if (!Read_Serial()) {
+    Serial.write(UNREC_MSG);
+  }
+  else {
+    Serial.print("Arr: {");
+    for (int i = 0;i < RANGE_DEGREES; i++){
+      Serial.print(Arr[i]);
+      Serial.print(",");
+    }
+    Serial.println("}");
+  }
   // Maths on the array
 }
 
-byte Read_Serial() {
-  int Index;
+bool Read_Serial() {
+  int Index = 0;
+  bool Serial_Status = true;
   byte Msg = 0;
-  if (Serial.Available() && Serial.read() == START_MSG) {
-    while (true) {
+  unsigned long t0 = millis();
+
+  while(!Serial.available()) {
+    if (millis() - t0 >= SERIAL_TIMEOUT) {
+      return false;
+    }
+  }
+  if (Serial.read() == START_MSG) {
+    while (Serial_Status) {
       while (!Serial.available()); // can be dangerous if gets stuck
       Msg = Serial.read();
       if (Msg == END_MSG) {
-        Serial.write(ACK_MSG);
+        Serial.write(ACK_MSG); 
         return true;
       }
       else {
@@ -49,4 +71,5 @@ byte Read_Serial() {
       }
     }
   }
+  return false;
 }
