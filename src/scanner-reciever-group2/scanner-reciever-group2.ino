b5c794c01c7ddf82facab2141398b259e351df55
Torin Stanton-Andersson <161447267+Tottocs@users.noreply.github.com>
2026-03-18 19:29:51 +0000

    Update scanner-reciever-group2.ino
    Minor changes

--- src/scanner-reciever-group2/scanner-reciever-group2.ino
+++ src/scanner-reciever-group2/scanner-reciever-group2.ino
@@ -21,7 +21,7 @@ void setup() {
   Serial.begin(UART_BAUDRATE);
   pinMode(INT_PIN, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  attachInterrupt(digitalPinToInterrupt(INT_PIN),ISR_Handler,HIGH);
+  attachInterrupt(digitalPinToInterrupt(INT_PIN), ISR_Handler, RISING);
 }
 
 void loop() {
@@ -51,18 +51,27 @@ bool Read_Serial() {
   bool Serial_Status = true;
   byte Msg = 0;
   unsigned long t0 = millis();
-
+  t0 = millis();
+  Serial.println(t0);
   while(!Serial.available()) {
+    Serial.println(millis() - t0);
     if (millis() - t0 >= SERIAL_TIMEOUT) {
       return false;
     }
   }
+  Serial.println("1");
   if (Serial.read() == START_MSG) {
     while (Serial_Status) {
-      while (!Serial.available()); // can be dangerous if gets stuck
+      t0 = millis();
+      while (!Serial.available()) {
+        if (millis() - t0 >= SERIAL_TIMEOUT) {
+          return false;
+        }
+      }; // can be dangerous if gets stuck
       Msg = Serial.read();
       if (Msg == END_MSG) {
-        Serial.write(ACK_MSG); 
+        Serial.write(ACK_MSG);
+        Serial_Status = false;
         return true;
       }
       else {
